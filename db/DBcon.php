85af91c48d73244b10725422cd4ccded579faae2
KavinduAluthwaththa <kabaluthwaththa@std.foc.sab.ac.lk>
2025-10-04 17:42:38 +0530

    Refactor database connection to use environment variables for configuration

--- db/DBcon.php
+++ db/DBcon.php
@@ -1,11 +1,17 @@
 <?php
-$servername = "localhost"; 
-$username = "root"; 
-$password = "18602"; 
-$database = "agriecom";
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$servername = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
+$database = $_ENV['DB_NAME'];
 
 $conn = new mysqli($servername, $username, $password, $database);
-
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
